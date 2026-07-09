bd2b4ffce1431dcfa328a49df24cd814eb2205e1
Keith Purvis <jaywill2kacc@gmail.com>
2026-07-08 20:32:58 -0400

    Initial Commit

---
+++ src/main/java/io/github/thedeveloperkeith/internshiptracker2027/InternshipTracker2027Application.java
@@ -0,0 +1,13 @@
+package io.github.thedeveloperkeith.internshiptracker2027;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipTracker2027Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipTracker2027Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/thedeveloperkeith/internshiptracker2027/InternshipTracker2027ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.thedeveloperkeith.internshiptracker2027;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipTracker2027ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
